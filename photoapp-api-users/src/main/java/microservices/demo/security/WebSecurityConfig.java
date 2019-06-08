0d61b88b29aa1849b022c980a84f697e2c243f11
chrisdoberman <chris.herb@gmail.com>
2019-06-08 15:04:31 -0400

    customized login url

--- photoapp-api-users/src/main/java/microservices/demo/security/WebSecurityConfig.java
+++ photoapp-api-users/src/main/java/microservices/demo/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package microservices.demo.security;
 
 import microservices.demo.users.services.UsersService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,6 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private Environment environment;
 
+    @Value("${gateway.ip}")
+    private String gatewayIp;
+
+    @Value("${login.url.path}")
+    private String loginUrlPath;
+
     public WebSecurityConfig(UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder,
                              Environment environment) {
         this.usersService = usersService;
@@ -28,9 +35,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        // restrict by ip address
-        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+        // restrict by ip address. can you restrict by service name? e.g. zuul-gateway
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(gatewayIp)
                 .and().addFilter(getAuthenticationFilter());
+
         //http.authorizeRequests().antMatchers("/users/**").permitAll();
         //allow h2-console frame for development
         http.headers().frameOptions().disable();
@@ -44,8 +52,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment,
                 authenticationManager());
+        // customize the login url path, default by spring is /login
+        authenticationFilter.setFilterProcessesUrl(loginUrlPath);
         //authenticationFilter.setAuthenticationManager(authenticationManager());
-        //authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
         return authenticationFilter;
     }
 }
